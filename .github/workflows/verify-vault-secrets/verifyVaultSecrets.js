3104a55279978c0c8f1fbe254b49cee416d75b5d
Conor McLaughlin <conor.mclaughlin@thescore.com>
2024-04-26 15:23:40 -0300

    debugging

--- .github/workflows/verify-vault-secrets/verifyVaultSecrets.js
+++ .github/workflows/verify-vault-secrets/verifyVaultSecrets.js
@@ -33,6 +33,11 @@ module.exports = async ({ github, context, core }) => {
   const checkVaultSecrets = async (environment, edge, service) => {
     const url = `${getVaultAddr(environment)}/v1/scorebet/subkeys/${service}/${environment}/${edge}`;
     console.log(getVaultToken(environment));
+    
+    getVaultToken(environment).forEach(element => {
+      console.log(element);
+    });
+
     try {
       const response = await axios({
         method: 'get',
